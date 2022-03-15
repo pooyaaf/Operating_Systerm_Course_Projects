614a327d33c396a223e4d37f09428a31952d08d9
pooyaaf <pooya1380@gmail.com>
2022-03-15 17:31:08 +0330

    init_change

--- Socket_programming/test_client.c
+++ Socket_programming/test_client.c
@@ -7,10 +7,14 @@
 #include <string.h>
 #include <arpa/inet.h>
 #include <sys/time.h>
-#include <signal.h>
 
 #define LocalPort 8082
 int roomPort = 0;
+int turns[3][3] = {
+    {1, 0, 0},
+    {0, 1, 0},
+    {0, 0, 1}};
+
 int connectServer(int port)
 {
     int fd;
@@ -30,6 +34,49 @@ int connectServer(int port)
 
     return fd;
 }
+
+void QRoom(int t1, int t2, int t3)
+{
+    int sock, broadcast = 1, opt = 1;
+    char buffer[1024] = {0};
+    struct sockaddr_in bc_address;
+
+    sock = socket(AF_INET, SOCK_DGRAM, 0);
+    setsockopt(sock, SOL_SOCKET, SO_BROADCAST, &broadcast, sizeof(broadcast));
+    setsockopt(sock, SOL_SOCKET, SO_REUSEPORT, &opt, sizeof(opt));
+
+    bc_address.sin_family = AF_INET;
+    bc_address.sin_port = htons(roomPort);
+    bc_address.sin_addr.s_addr = inet_addr("192.168.1.255");
+
+    bind(sock, (struct sockaddr *)&bc_address, sizeof(bc_address));
+
+    while (1)
+    {
+        memset(buffer, 0, 1024);
+        for (int i = 0; i < 3; i++)
+        { //for first q
+            for (int j = 0; j < 3; j++)
+            { // ask
+                for (int z = 0; z < 3; z++)
+                { //answer
+                    if (turns[j][z] == 1)
+                    {
+                        printf("turn for %d of list:\n", z + 1);
+                        read(0, buffer, 1024);
+                        int a = sendto(sock, buffer, strlen(buffer), 0, (struct sockaddr *)&bc_address, sizeof(bc_address));
+                    }
+                    if (turns[j][z] == 0)
+                    {
+                        recv(sock, buffer, 1024, 0);
+                        printf("%s\n", buffer);
+                    }
+                }
+            }
+        }
+    }
+}
+
 int main(int argc, char const *argv[])
 {
     int fd;
@@ -49,20 +96,19 @@ int main(int argc, char const *argv[])
     recv(fd, buff, 1024, 0);
     printf("Server said: %s\n", buff);
     memset(buff, 0, 1024);
-    int turn1, turn2;
+    int turn1, turn2, turn3;
     while (1)
     {
         read(0, buff, 1024);
         send(fd, buff, strlen(buff), 0);
         printf("Wait for Server response!\n");
-        printf("roomport is: %d",roomPort);
         recv(fd, buff, 1024, 0);
         roomPort = atoi(&buff[0]);
-        printf("roomport is: %d",roomPort);
         if (roomPort > LocalPort)
         {
             turn1 = atoi(&buff[4]);
             turn2 = atoi(&buff[6]);
+            turn3 = atoi(&buff[8]);
             printf("Server said: on Port %d: Welcome to your question room! \n ", roomPort);
             break;
         }
@@ -74,7 +120,8 @@ int main(int argc, char const *argv[])
     }
     if (roomPort > 0)
     {
-        printf("Turns: %d  %d! \n ", turn1, turn2);
+        printf("Turns: %d %d %d! \n ", turn1, turn2, turn3);
+        QRoom(turn1, turn2, turn3);
     }
 
     return 0;
