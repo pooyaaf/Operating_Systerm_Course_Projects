7cf7415e7fb33f907fe63e3d4db5c18ab84f3332
pooyaaf <pooya1380@gmail.com>
2022-04-20 11:28:17 +0430

    init

---
+++ Process_management_and_communication/code/meanCalculator.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <dirent.h>
+
+
+
+int main(int argc, char *argv[])
+{
+    printf("%s\n",argv[1]);
+    return 0;
+}
